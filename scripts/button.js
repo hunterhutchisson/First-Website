e961d1ea49671cc19fcaacd4c24105503932cb3c
hunterhutchisson <hunter.hutchisson@gmail.com>
2021-09-02 12:49:06 -0500

    fixed local storage

--- scripts/button.js
+++ scripts/button.js
@@ -2,22 +2,23 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if(!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = 'Feel tree to look around, ' + myName;
-    }
-  }
-
-  if(!localStorage.getItem('name')) {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
     setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Feel tree to look around, ' + myName;
+  }  
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
 } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Feel tree to look around, ', + myName;
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Feel tree to look around, ' + storedName;
 }
   
 myButton.onclick = function() {
   setUserName();
 }
+
